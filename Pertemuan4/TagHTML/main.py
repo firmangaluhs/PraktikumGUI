5ddb66b02c6831904ebbc65b416803bc2f0d0c82
Firman Galuh Sembodo <44075596+firmangaluhs@users.noreply.github.com>
2019-04-16 21:23:37 +0700

    Add files via upload

---
+++ Pertemuan4/TagHTML/main.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from TextForm import *
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+    minform = TextForm()
+    minform.show()
+    a.exec_()
